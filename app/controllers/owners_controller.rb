f1460451d27f461ca4202f2556edb950ec5e34c1
Bailey Cage <bailey.cage@gmail.com>
2022-10-25 15:33:08 -0400

    add filter for name parameter

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,6 +2,11 @@ class OwnersController < ApplicationController
 
    def index
       owners = Owner.all
+      # iterate thru optional search parameters
+      if (params.keys.length > 2) then owners = filter_by_params owners end
+      # sort alphabetically by last then first
+      owners = owners.sort_by { |o| [o.last_name, o.first_name] }
+      # render & return
       render json: owners, status: :ok
    end
 
@@ -11,6 +16,16 @@ class OwnersController < ApplicationController
    end
 
    private
+
+   def filter_by_params owners
+      params.each do |key, value|
+         case key
+         when "name"
+            owners = owners.filter { |o| o.full_name.upcase.include? value.upcase}
+         end
+      end
+      owners
+   end
 
    def find_owner
       Owner.find(params[:id])

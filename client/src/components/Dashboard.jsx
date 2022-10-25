9eef90827345274e332b8786f2a4866ba31a56e7
Bailey Cage <bailey.cage@gmail.com>
2022-10-24 21:22:25 -0400

    redirect if user not logged in

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Header from './Header'
 
 function Dashboard (props) {
-   const { onLogout } = props
+   const { onLogout, user } = props
+
+   const navigate = useNavigate()
+
+   useEffect(() => {
+      if (!user) { navigate('/') }
+   }, [])
+
    return (<>
       <Header onLogout={onLogout} />
       <Outlet />

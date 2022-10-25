35daf6d80ebc3da41cc56b8729ffd0c71420194a
Bailey Cage <bailey.cage@gmail.com>
2022-10-24 22:29:37 -0400

    remove redirect due to bug

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,15 +1,9 @@
-import React, { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Header from './Header'
 
 function Dashboard (props) {
    const { onLogout, user } = props
-
-   const navigate = useNavigate()
-
-   useEffect(() => {
-      if (!user) { navigate('/') }
-   }, [])
 
    return (<>
       <Header onLogout={onLogout} />

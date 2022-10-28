9e00e540efac4acd2f552fd6320e1086384d9b49
Bailey Cage <bailey.cage@gmail.com>
2022-10-27 23:34:29 -0400

    group similar buttons into dropdown

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -12,16 +12,56 @@ function NavBar (props) {
       }).then(onLogout)
    }
 
+   function handleNav (eleId, path) {
+      document.getElementById(eleId).classList.remove("show")
+      navigate(path)
+   }
+
+   function toggleDropdown (eleId) {
+      document.getElementById(eleId).classList.toggle("show")
+   }
+
    return (
       <nav className="navbar">
-         <button onClick={() => navigate("dashboard")}>dashboard</button>
-         <button onClick={() => navigate("owners")}>owners</button>
-         <button onClick={() => navigate("owners/new")}>new owner</button>
-         <button onClick={() => navigate("pets")}>pets</button>
-         <button onClick={() => navigate("pets/new")}>new pet</button>
-         <button onClick={() => navigate("visits")}>today's visits</button>
-         <button onClick={() => navigate("visits/new")}>new visit</button>
-         <button type="button" onClick={() => handleLogout()}>logout</button>
+
+         <div className="navbar-cell">
+            <button className="navbar-button" onClick={() => navigate("dashboard")}>dashboard</button>
+         </div>
+
+         <div className="navbar-cell">
+            <div className="dropdown navbar-button">
+               <div className='dropdown-button'>owners</div>
+               <div id='navOwnersDropdown' className='dropdown-content'>
+                  <div className='dropdown-button' onClick={() => handleNav("navOwnersDropdown", "owners/new")}>new owner</div>
+                  <div className='dropdown-button' onClick={() => handleNav("navOwnersDropdown", "owners")}>view all</div>
+               </div>
+            </div>
+         </div>
+
+         <div className="navbar-cell">
+            <div className="dropdown navbar-button">
+               <div className='dropdown-button' onClick={() => toggleDropdown('navPetsDropdown')}>pets</div>
+               <div id='navPetsDropdown' className='dropdown-content'>
+                  <div className='dropdown-button' onClick={(e) => handleNav("navPetsDropdown", "pets/new")}>new pet</div>
+                  <div className='dropdown-button' onClick={(e) => handleNav("navPetsDropdown", "pets")}>view all</div>
+               </div>
+            </div>
+         </div>
+
+         <div className="navbar-cell">
+            <div className="dropdown navbar-button">
+               <div className='dropdown-button' onClick={() => toggleDropdown('navVisitsDropdown')}>visits</div>
+               <div id='navVisitsDropdown' className='dropdown-content'>
+                  <div className='dropdown-button' onClick={(e) => handleNav("navVisitsDropdown", "visits")}>today's visits</div>
+                  <div className='dropdown-button' onClick={(e) => handleNav("navVisitsDropdown", "visits/new")}>new visit</div>
+                  <div className='dropdown-button' onClick={(e) => handleNav("navVisitsDropdown", "visits/all")}>view all</div>
+               </div>
+            </div>
+         </div>
+
+         <div className="navbar-cell">
+            <button type="navbar-button" onClick={() => handleLogout()}>logout</button>
+         </div>
       </nav>
    )
 }
